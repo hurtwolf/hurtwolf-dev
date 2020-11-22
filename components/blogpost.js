d4c5844acb6029da37701df945c836afa55b610f
Mikkel <mikkelkulk@gmail.com>
2020-11-22 14:59:39 +0100

    leftover changes

--- components/blogpost.js
+++ components/blogpost.js
@@ -2,16 +2,25 @@ import Lodash from 'lodash'
 import Moment from 'moment'
 
 export default function BlogPost({post}) {
+  const postType = () => {
+    if(post.metadata.video){
+      return <div style={{paddingTop: '56.25%'}} className="post-content mt-6 bg-gradient-to-b from-gray-200 py-4 px-4 rounded text-gray-700 relative overflow-hidden" dangerouslySetInnerHTML={{ __html: Lodash.unescape(post.content) }} />
+    }
+    if(post.metadata.soundcloud){
+      return <div style={{paddingTop: '56.25%'}} className="post-content mt-6 bg-gradient-to-b from-gray-200 py-4 px-4 rounded text-gray-700 relative overflow-hidden" dangerouslySetInnerHTML={{ __html: Lodash.unescape(post.content) }} />
+    }
+    return <div className="post-content mt-6 bg-gradient-to-b from-gray-200 py-4 px-4 rounded text-gray-700 relative overflow-hidden" dangerouslySetInnerHTML={{ __html: Lodash.unescape(post.content) }} />
+  }
   return (
     <div className="post">
       <div className="flex post-header">
-        {post.thumbnail ? <img className="mr-4 rounded-full w-12 h-12" alt={post.title} src={post.thumbnail} /> : null}
+        {post.thumbnail ? <div className="bg-cover bg-center h-12 w-12 rounded-full mr-4" style={{ backgroundImage: `url('${post.thumbnail}')` }} /> : null}
         <div>
           <p className="text-lg font-semibold">{post.title}</p>
           <p className="text-gray-500 text-sm">{Moment(post.created_at).fromNow()} by {post.metadata.author.title}</p>
         </div>
       </div>
-      <div className="post-content mt-6 bg-gradient-to-b from-gray-200 py-4 px-6 rounded text-gray-700" dangerouslySetInnerHTML={{ __html: Lodash.unescape(post.content) }}></div>
+      {postType()}
     </div>
   )
 }
